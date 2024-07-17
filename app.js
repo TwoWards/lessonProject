dab6ecd2ce921b9072ac679f2ee9552ac64fb4ce
TwoWards <yoberzerkwolf@gmail.com>
2024-07-17 14:33:42 +0400

    feat: разработать эндпоинты GET, POST, DELETE
    Добавлены эндпоинты: GET для получения списка уроков, POST для создания нового урока и DELETE для удаления уровка.
    Добавлена обработка ошибок при старте приложения.

--- app.js
+++ app.js
@@ -20,4 +20,50 @@ async function startApp () {
     }
 }
 
-startApp();
+app.get('/api/lessons', async (req, res) => {
+    try {
+        const lessons = await Lesson.find({});
+        res.send(lessons);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+app.post('/api/lessons', async (req, res) => {
+    const {name, path, numbersArray} = req.body;
+
+    if (!req.body) return res.sendStatus(400);
+    if (!name || !path || !Array.isArray(numbersArray)) return res.sendStatus(400);
+
+    try {
+        const lesson = new Lesson({name, path, numbersArray});
+
+        await lesson.save();
+        res.status(201).send(lesson);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+app.delete('/api/lessons/:id', async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const lesson = await Lesson.findByIdAndDelete(id);
+
+        if (!lesson) {
+            return res.sendStatus(404);
+        } else {
+            return res.send(lesson);
+        }
+    } catch (err) {
+        console.log('Ошибка при удалении урока:', err);
+        return res.sendStatus(500);
+    }
+});
+
+startApp().catch(err => {
+    console.error('Ошибка при запуске приложения', err);
+});
